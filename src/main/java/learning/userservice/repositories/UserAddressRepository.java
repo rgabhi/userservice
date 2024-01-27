e6c7d07b8ed590ee3402c3a6f15e57f685f28664
Abhinav Gupta <abhinavrgupta98@gmail.com>
2024-01-27 20:06:52 +0530

    add user repos

---
+++ src/main/java/learning/userservice/repositories/UserAddressRepository.java
@@ -0,0 +1,9 @@
+package learning.userservice.repositories;
+
+import learning.userservice.models.UserAddress;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserAddressRepository extends JpaRepository<UserAddress, Long> {
+}
---
+++ src/main/java/learning/userservice/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package learning.userservice.repositories;
+
+import learning.userservice.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
