3e7929fa0d546c557d0d14c80bd92426483b5fb8
Abhinav Gupta <abhinavrgupta98@gmail.com>
2024-01-27 14:18:23 +0530

    add baseModel clas

---
+++ src/main/java/learning/userservice/models/BaseModel.java
@@ -0,0 +1,21 @@
+package learning.userservice.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseModel{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    long id;
+    Date createdAt;
+    Date updatedAt;
+    boolean isDeleted;
+}
