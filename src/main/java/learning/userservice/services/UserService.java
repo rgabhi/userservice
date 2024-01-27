98424d1a626042aa394b358ae23ffc379697779f
Abhinav Gupta <abhinavrgupta98@gmail.com>
2024-01-27 20:03:43 +0530

    add userSerivce methods

--- src/main/java/learning/userservice/services/UserService.java
+++ src/main/java/learning/userservice/services/UserService.java
@@ -1,5 +1,14 @@
 package learning.userservice.services;
 
+import learning.userservice.models.User;
+
+import java.util.List;
+
 public interface UserService {
-    
+    public User getUser(Long id);
+    public List<User> getAllUsers();
+    public User addUser(User user);
+    public User updateUser(Long id, User user);
+    public void deleteUser(Long id);
+
 }
