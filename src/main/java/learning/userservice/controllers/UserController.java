63e124a12a5f33b03e5a79e32bfecdb4bca27511
Abhinav Gupta <abhinavrgupta98@gmail.com>
2024-01-27 19:18:47 +0530

    add api methods to user controller

--- src/main/java/learning/userservice/controllers/UserController.java
+++ src/main/java/learning/userservice/controllers/UserController.java
@@ -1,4 +1,42 @@
 package learning.userservice.controllers;
 
+import learning.userservice.dtos.UserDto;
+import learning.userservice.models.User;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUser(@PathVariable(name="id") Long id){
+        return  null;
+    }
+    @GetMapping()
+    public ResponseEntity<List<UserDto>> getAllUsers(){
+        return  null;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDto> addUser(@RequestBody UserDto userDto){
+        return null;
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable(name="id") Long id, @RequestBody UserDto userDto){
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void>  deleteUser(@PathVariable(name="id") Long id){
+        return null;
+    }
+
+
+
+
+
 }
