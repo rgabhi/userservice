075b1045f055719bebd4d194f2b1063c6bc54349
Abhinav Gupta <abhinavrgupta98@gmail.com>
2024-01-28 10:50:21 +0530

    rename to Fullname

--- src/main/java/learning/userservice/models/FullName.java
+++ src/main/java/learning/userservice/models/FullName.java
@@ -1,15 +1,13 @@
 package learning.userservice.models;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.util.StringUtils;
 
 @Getter
 @Setter
 @Entity
-public class UserName extends BaseModel{
+public class FullName extends BaseModel{
     String firstName;
     String lastName;
 }
--- src/main/java/learning/userservice/repositories/UserNameRepository.java
+++ src/main/java/learning/userservice/repositories/UserNameRepository.java
@@ -1,10 +1,10 @@
 package learning.userservice.repositories;
 
-import learning.userservice.models.UserName;
+import learning.userservice.models.FullName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserNameRepository extends JpaRepository<UserName, Long> {
+public interface UserNameRepository extends JpaRepository<FullName, Long> {
 
 }
