569fa5d27e25c321cc5ecb27b66e3495696b6664
Abhinav Gupta <abhinavrgupta98@gmail.com>
2024-01-28 10:46:31 +0530

    implement UserBuilder class

--- src/main/java/learning/userservice/models/User.java
+++ src/main/java/learning/userservice/models/User.java
@@ -4,20 +4,72 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.List;
 
 @Getter
-@Setter
 @Entity
 public class User extends BaseModel{
     String email;
     String username;
     String password;
     @ManyToOne
-    UserName userName;
+    FullName fullName;
     @ManyToMany
     List<UserAddress> userAddresses;
     String phone;
+    private User(UserBuilder userBuilder){
+        super();
+        this.email = userBuilder.email;
+        this.username = userBuilder.username;
+        this.password = userBuilder.password;
+        this.fullName = userBuilder.fullName;
+        this.userAddresses = userBuilder.userAddresses;
+        this.phone = userBuilder.phone;
+    }
+
+    public User() {
+
+    }
+
+
+    public static class UserBuilder{
+        String email;
+        String username;
+        String password;
+        FullName fullName;
+        List<UserAddress> userAddresses;
+        String phone;
+
+        public  UserBuilder setEmail(String email){
+            this.email = email;
+            return this;
+        }
+        public  UserBuilder setUsername(String username){
+            this.username = username;
+            return this;
+        }
+        public  UserBuilder setPassword(String password){
+            this.password = password;
+            return this;
+        }
+
+        public  UserBuilder setFullName(FullName name){
+            this.fullName = name;
+            return  this;
+        }
+        public UserBuilder setAddresses(List<UserAddress> addresses){
+            this.userAddresses = addresses;
+            return this;
+        }
+        public UserBuilder setPhone(String phone){
+            this.phone = phone;
+            return this;
+        }
+
+        public User build(){
+            return new User(this);
+        }
+
+    }
 }
