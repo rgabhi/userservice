f644d7587902d9f0d517316262a278c96b254a5e
Abhinav Gupta <abhinavrgupta98@gmail.com>
2024-01-30 07:28:25 +0530

    remove repos

--- src/main/java/learning/userservice/repositories/GeolocationRepository.java
+++
@@ -1,9 +0,0 @@
-package learning.userservice.repositories;
-
-import learning.userservice.models.Geolocation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GeolocationRepository extends JpaRepository<Geolocation, Long> {
-}
