f713075eda4c87e381f31dc644dc4af9756e6bba
Abhinav Gupta <abhinavrgupta98@gmail.com>
2024-03-07 07:55:43 +0530

    init flyway migration

---
+++ src/main/resources/db/migration/V1__.sql
@@ -0,0 +1,71 @@
+CREATE TABLE address
+(
+    id         BIGINT NOT NULL,
+    created_at datetime NULL,
+    updated_at datetime NULL,
+    is_deleted BIT(1) NOT NULL,
+    user_id    BIGINT NULL,
+    city       VARCHAR(255) NULL,
+    street     VARCHAR(255) NULL,
+    number     INT NULL,
+    zipcode    VARCHAR(255) NULL,
+    longitude  VARCHAR(255) NULL,
+    latitude   VARCHAR(255) NULL,
+    CONSTRAINT pk_address PRIMARY KEY (id)
+);
+
+CREATE TABLE `role`
+(
+    id         BIGINT NOT NULL,
+    created_at datetime NULL,
+    updated_at datetime NULL,
+    is_deleted BIT(1) NOT NULL,
+    name       VARCHAR(255) NULL,
+    CONSTRAINT pk_role PRIMARY KEY (id)
+);
+
+CREATE TABLE token
+(
+    id         BIGINT NOT NULL,
+    created_at datetime NULL,
+    updated_at datetime NULL,
+    is_deleted BIT(1) NOT NULL,
+    value      VARCHAR(255) NULL,
+    user_id    BIGINT NULL,
+    expiry_at  datetime NULL,
+    CONSTRAINT pk_token PRIMARY KEY (id)
+);
+
+CREATE TABLE user
+(
+    id                BIGINT NOT NULL,
+    created_at        datetime NULL,
+    updated_at        datetime NULL,
+    is_deleted        BIT(1) NOT NULL,
+    email             VARCHAR(255) NULL,
+    is_email_verified BIT(1) NOT NULL,
+    username          VARCHAR(255) NULL,
+    hashed_password   VARCHAR(255) NULL,
+    first_name        VARCHAR(255) NULL,
+    last_name         VARCHAR(255) NULL,
+    phone             VARCHAR(255) NULL,
+    CONSTRAINT pk_user PRIMARY KEY (id)
+);
+
+CREATE TABLE user_roles
+(
+    user_id  BIGINT NOT NULL,
+    roles_id BIGINT NOT NULL
+);
+
+ALTER TABLE address
+    ADD CONSTRAINT FK_ADDRESS_ON_USER FOREIGN KEY (user_id) REFERENCES user (id);
+
+ALTER TABLE token
+    ADD CONSTRAINT FK_TOKEN_ON_USER FOREIGN KEY (user_id) REFERENCES user (id);
+
+ALTER TABLE user_roles
+    ADD CONSTRAINT fk_userol_on_role FOREIGN KEY (roles_id) REFERENCES `role` (id);
+
+ALTER TABLE user_roles
+    ADD CONSTRAINT fk_userol_on_user FOREIGN KEY (user_id) REFERENCES user (id);
